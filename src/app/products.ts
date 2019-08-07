6083671fc9c4714f8e5790966e7996e6b29b5135
MkTavish <wola.adedeji@gmail.com>
2019-08-07 09:01:00 +0100

    Product details component added, Routing configured

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,24 +1,19 @@
 export const products = [
   {
-    name: 'Phone XL',
+    name: 'IPhone X',
     price: 799,
     description: 'A large phone with one of the best screens'
   },
   {
-    name: 'Phone Mini',
+    name: 'IPhone XL',
     price: 699,
     description: 'A great phone with one of the best cameras'
   },
   {
-    name: 'Phone Standard',
+    name: 'IPhone 8',
     price: 299,
     description: ''
   }
 ];
 
 
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
